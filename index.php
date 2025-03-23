cb64734f2ec98dcbe7f2e2db45404ab82fa1e18f
Vinicius Paiva <viniciusspaiva1628@gmail.com>
2025-03-23 03:05:42 -0300

    PRIMEIRO COMMIT

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>PRIMEIRA AULA</title>
+</head>
+
+<body>
+    <form action="teste.php" method="post">
+
+        Nome: <input type="text" name="nome"> <!--CAMPO NOME COM TIPO TEXTO E NAME = NOME-->
+        Idade: <input type="text" name="idade">
+        <input type="submit" value="validar">
+
+        
+
+    </form>
+</body>
+
+</html>
+
