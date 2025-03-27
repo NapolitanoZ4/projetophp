b95823a50a33ccc29e85425f056d37be30b641f6
5163783 <5163783@liae.edu.uniube.br>
2025-03-26 21:58:45 -0300

    atualização html

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
        CPF: <input type="text" name="cpf"><br><br> <!--CAMPO NOME COM TIPO TEXTO E NAME = usuario-->
        SENHA: <input type="password" name="senha"><br><br>
         <input type="submit" value="ENVIAR"> <!--BOTAO QUE ENVIA DADOS USANDO O METODO POST-->
-    </form>
+</form>
 
 </body>
 
-</html>
+</html
--- principal.php
+++ principal.php
@@ -5,3 +5,21 @@ echo "CPF: ".$_SESSION['cpf'].'<br>';
 echo "NOME: ".$_SESSION['nome'].'<br>';
 echo "SENHA: ".$_SESSION['senha'].'<br>';
 ?>
+
+<div style="width: 800px; margin: 0 auto;";>
+        <div style="min-height: 100px; width: 100%; background-color: #4caf50";>
+            <div style="width: 50%; float: left">
+                <span style="padding-left: 10px;"> Olá <?=$_SESSION['cpf'];?></span>
+            </div>
+
+            <div style="width: 50%; float:left; text-align:right;">
+                <span style="background-color:blue; margin-right: 10px;"><a href="sair.php">
+                    <font color="black">SAIR</font></a></span>
+            </div>
+        </div>
+        <div id="menu" style="width: 200px; background-color: #f4f4f4; min-height: 400px; float: left;">
+            <h2>MENU</h2>
+            
+        </div>
+
+    </div>
